a192a9e90bd067367b0627228a6247a7141a1aa4
Mark Mwima <markmwimar@gmail.com>
2024-08-15 16:24:21 +0300

    importing package

---
+++ src/DemoJDBC.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+public class DemoJDBC {
+    public static void main(String[] args) {
+        /*
+        import package
+        load connection
+        create connection
+        create statement
+        execute statement
+        process statement
+        close
+        */
+        //
+    }
+}
